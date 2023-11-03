83aa379503f49da3471ed143bf652a63e80cd633
Victor <vnjorogen2001@gmail.com>
2023-11-03 09:19:05 +0300

    :sparkles: feat(component): create 'about', 'invest' and 'social_impact' components This commit adds a new Angular component named 'about', 'invest' and 'social_impact' to the project. This component will serve within the application.

--- elewa_group/src/app/app.module.ts
+++ elewa_group/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { InvestComponent } from './invest/invest.component';
+import { SocialImpactComponent } from './social-impact/social-impact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    InvestComponent,
+    SocialImpactComponent
   ],
   imports: [
     BrowserModule,
